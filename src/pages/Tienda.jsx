7db3a8e82d3b0e69d8ccb70827f861bc673cc722
Belén <belen.munoz@usach.cl>
2024-07-05 10:37:46 -0400

    add: tarjetas con material UI y map

--- src/pages/Tienda.jsx
+++ src/pages/Tienda.jsx
@@ -1,9 +1,14 @@
 import Hero from "../components/Hero/Hero";
+import Cards from "../components/Cards/Cards";
+import tendencias from "../components/assets/data";
+import nuevaColeccion from "../components/assets/new_collections";
 
 function Tienda() {
   return (
     <>
       <Hero />
+      <Cards data={tendencias} titulo="Tendencias" />
+      <Cards data={nuevaColeccion} titulo="Nueva colección" />
     </>
   );
 }
